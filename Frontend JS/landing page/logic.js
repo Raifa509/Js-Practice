fc8921802142d5213b2e00ecd7f545a9c833801d
Raifa509 <fathimathulraifanp@gmail.com>
2025-07-23 20:04:22 +0400

    23/7/25

--- Frontend JS/landing page/logic.js
+++ Frontend JS/landing page/logic.js
@@ -1,72 +1,75 @@
-class Landing{
+class Landing {
     // properties
-    database={
-        'milan':{username:'milan',password:'milan123'},
-        'ayra':{username:'ayra',password:'ayra123'},
-        'ajmal':{username:'ajmal',password:'ajmal123'},
+    database = {
+        'milan': { username: 'milan', password: 'milan123' },
+        'ayra': { username: 'ayra', password: 'ayra123' },
+        'ajmal': { username: 'ajmal', password: 'ajmal123' },
     }
     // method
-    saveData(){
-        if(this.database)
-        {
-             localStorage.setItem('database',JSON.stringify(this.database))
-        } 
+    saveData() {
+        if (this.database) {
+            localStorage.setItem('database', JSON.stringify(this.database))
+        }
     }
     //method-get data
-    getData(){
-        const content=JSON.parse(localStorage.getItem('database'))
-        console.log(content);
+    getData() {
+
+        this.database = JSON.parse(localStorage.getItem('database'));
     }
-    //method - register
-    register(){
+    register() {
         console.log(regUser.value);
         console.log(regPass.value);
-        if(regUser.value=='' || regPass.value=='')
-        {
+        if (regUser.value == '' || regPass.value == '') {
             alert('Please fill the form completely')
         }
-        else{
+        else {
             this.getData()
-            if(regUser.value in this.database){
-            alert('User already exist.Please login')
-             window.location='login.html'
+            if (regUser.value in this.database) {
+                alert('User already exist.Please login')
+                window.location = 'login.html'
+            }
+            else {
+                this.database[regUser.value] = { username: regUser.value, password: regPass.value }
+                this.saveData()
+                alert('Registration Successfull')
+                window.location = 'login.html'
+            }
         }
-        else{
-            this.database[regUser.value]={username:regUser.value,password:regPass.value}
-            this.saveData()
-            alert('Registration Successfull')
-            window.location='login.html'
-        }
-        }
-        
-    }
 
-    //method-login
-    login(){
+    }
+    login() {
         console.log(loginUser.value);
         console.log(loginPass.value);
-        if(loginUser.value=='' || loginPass.value=='')
-        {
+        if (loginUser.value == '' || loginPass.value == '') {
             alert('Please fill the form completely')
         }
-        else{
+        else {
             this.getData()
-            if(loginUser.value in this.database){
-                if(this.database[loginUser.value].password)
-                {
-                    if(loginUser.value==this.database.username && loginPass.value==this.database.password)
-                    {
-                        alert('login success')
-                        window.location='home.html'
-                    }
+            if (loginUser.value in this.database) {
+                if (this.database[loginUser.value].password == loginPass.value) {
+                    localStorage.setItem("user", loginUser.value)
+                    alert('login successfully')
+                    window.location = 'home.html'
+
+                }
+                else {
+                    alert('incorrect username or password')
                 }
             }
-            else{
+            else {
                 alert('User doesnot exist')
             }
         }
-        
+
     }
+
+
 }
-const obj=new Landing()
+
+const obj = new Landing()
 obj.getData()
+
+const user = localStorage.getItem("user");
+if (user) {
+    document.getElementById("userdisplay").textContent = user;
+}
